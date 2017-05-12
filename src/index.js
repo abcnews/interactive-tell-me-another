d805ca6b0b3a638e2637dc4e2f0511b38bd668ff
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-05-12 10:29:39 +1000

    Give unique ids to headings and update the location hash as we click buttons. We can use this to pre-reveal preceding stories on refresh and maintain scroll position (including when navigating back from another page).

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ const slice = Array.prototype.slice;
 
 const SCROLL_DURATION = 1000;
 const SCROLL_EL_Y_OFFSET = 64;
+const NON_LETTERS_PATTERN = /[^a-zA-Z]+/g;
 
 const storyEl = document.querySelector(`.Main, ${ns('story')}`);
 const childEls = slice.call(storyEl.children);
@@ -44,21 +45,40 @@ const endEl = storyEl.querySelector('a[name="endtellmeanother"]');
 
 if (endEl !== null && endEl.parentElement === storyEl) {
   const endElIndex = childEls.indexOf(endEl);
-
-  childEls
+  const headingEls = childEls
   .filter((el, index) => {
     return el.tagName === 'H2' && index < endElIndex;
   })
+  .map(el => {
+    el.id = el.textContent.replace(NON_LETTERS_PATTERN, '').toLowerCase();
+    return el;
+  });
+
+  const targetEl = document.getElementById(window.location.hash.slice(1));
+  const targetElIndex = (
+    targetEl !== null &&
+    targetEl.parentElement === storyEl
+  ) ? childEls.indexOf(targetEl) : -1;
+
+  headingEls
   .slice(1)
   .forEach(el => {
     const buttonEl = document.createElement('button');
 
-    buttonEl.className = 'TellMeAnotherButton is-unused';
+    buttonEl.className = 'TellMeAnotherButton'
+
+    if (childEls.indexOf(el) > targetElIndex) {
+      buttonEl.classList.add('is-unused');
+    }
+
     buttonEl.textContent = 'Tell me another…';
+
     buttonEl.onclick = () => {
       buttonEl.classList.remove('is-unused'); 
+      history.replaceState(null, null, `#${el.id}`);
       scrollToEl(el);
     };
+
     storyEl.insertBefore(buttonEl, el);
   });
 }
